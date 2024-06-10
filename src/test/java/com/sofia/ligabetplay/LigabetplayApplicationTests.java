4b4d5ed4d97ea849539eed35b45ab1ea8d2b7b67
S0fiaMedina <151967289+S0fiaMedina@users.noreply.github.com>
2024-06-10 14:46:42 -0500

    feat: :tada: initial commit - this commit implements the basic structure for the project - my first project with spring + thymeleaf + mySQL :D

---
+++ src/test/java/com/sofia/ligabetplay/LigabetplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofia.ligabetplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigabetplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
